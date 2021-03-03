f5c7609cafae8446b09cddce0096daf9f444b707
ch8n <chetan.garg36@gmail.com>
2021-03-03 15:29:49 +0530

    :bento: added fontstyles

--- app/src/main/java/dev/ch8n/pubdopter/ui/theme/Theme.kt
+++ app/src/main/java/dev/ch8n/pubdopter/ui/theme/Theme.kt
@@ -17,7 +17,8 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = White2
 
     /* Other default colors to override
 background = Color.White,
--- app/src/main/java/dev/ch8n/pubdopter/ui/theme/Type.kt
+++ app/src/main/java/dev/ch8n/pubdopter/ui/theme/Type.kt
@@ -2,29 +2,52 @@ package dev.ch8n.pubdopter.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.ch8n.pubdopter.R
+
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold)
+)
 
 // todo update current typography
 // learn from => https://github.com/TheCodeMonks/TopCorn2/blob/master/app/src/main/java/com/theapache64/topcorn2/ui/theme/Typography.kt
 // Set of Material typography styles to start with
 val Typography = Typography(
-        body1 = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp
-        )
-        /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = sp24
+    ),
+    h3 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = sp16
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = sp10
+    ),
+    body1 = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = sp14
+    ),
+    body2 = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = sp12
+    ),
+    button = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = sp16
+    ),
 )
